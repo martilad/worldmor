b805d5bf3ed30d9acc125f9ae73f36fcff5e1d49
Ladislav Martinek <martilad@fit.cvut.cz>
2019-01-15 16:17:22 +0100

    create resizing of map for create an illusion of endless map

--- worldmor/worldmor.pyx
+++ worldmor/worldmor.pyx
@@ -42,37 +42,6 @@ cdef class Worldmor:
     def __dealloc__(self):
         free_mem(self.map, self.rows)
 
-    @cython.wraparound(False)
-    @cython.boundscheck(False)
-    cpdef np.ndarray[np.int64_t, ndim=2] get_map(self, int row, int col):
-        """Return map"""
-        cdef int i, j
-        cdef np.ndarray[np.int64_t, ndim=2] map_view = np.zeros((row, col), dtype=np.int64)
-        cdef coords co = self.get_cords_slides(row, col)
-        #print(co)
-        if co.col_min < 1 or co.col_max >= self.cols:
-            print("need add cols")
-        if co.row_min < 1 or co.row_max >= self.rows:
-            print("need add rows")
-        # TODO: check if is to big or small it is need resize (some magic)
-        for i in range(row):
-            for j in range(col):
-                if co.row_min+i == self.pos_row and co.col_min+j == self.pos_col:
-                    self.map[i][j] = 0
-                else:
-                    map_view[i, j] = self.map[co.row_min+i][co.col_min+j]
-        return map_view
-
-    @cython.nonecheck(False)
-    @cython.cdivision(True)
-    cdef coords get_cords_slides(self, int x, int y):
-        cdef int x1, x2, y1, y2
-        x1 = self.pos_row - (x-1)/2
-        x2 = self.pos_row + x/2 + 1
-        y1 = self.pos_col - (y-1)/2
-        y2 = self.pos_col + y/2 + 1
-        return coords(x1, x2, y1, y2)
-
     cpdef void left(self):
         self.pos_col -= 1
 
@@ -84,6 +53,111 @@ cdef class Worldmor:
 
     cpdef void down(self):
         self.pos_row += 1
+
+    @cython.wraparound(False)
+    @cython.boundscheck(False)
+    cpdef np.ndarray[np.int64_t, ndim=2] get_map(self, int row, int col):
+        """Return map"""
+        cdef int i, j
+        cdef np.ndarray[np.int64_t, ndim=2] map_view = np.zeros((row, col), dtype=np.int64)
+        cdef coords co = self.get_cords_slides(row, col)
+
+        if co.col_min < 1:
+            self.refactor_cols_down()
+            co = self.get_cords_slides(row, col)
+        if co.col_max >= self.cols:
+            self.refactor_cols_up()
+            co = self.get_cords_slides(row, col)
+        if co.row_min < 1:
+            self.refactor_rows_down()
+            co = self.get_cords_slides(row, col)
+        if co.row_max >= self.rows:
+            self.refactor_rows_up()
+            co = self.get_cords_slides(row, col)
+
+        for i in range(row):
+            for j in range(col):
+                if co.row_min+i == self.pos_row and co.col_min+j == self.pos_col:
+                    map_view[i, j] = 0
+                else:
+                    map_view[i, j] = self.map[co.row_min+i][co.col_min+j]
+        return map_view
+
+    cdef void refactor_cols_up(self):
+        cdef int new_cols = self.cols * 2
+        cdef int * tmp
+        cdef int i, j
+
+        for i in range(self.rows):
+            tmp = <int *> PyMem_Malloc(new_cols*sizeof(int))
+            for j in range(self.cols):
+                tmp[j] = self.map[i][j]
+            for j in range(self.cols, new_cols, 1):
+                # TODO: need generate map base on some probability (no cycle of wall check)
+                tmp[j] = (rand() % 3) + 1
+            PyMem_Free(self.map[i])
+            self.map[i] = tmp
+        self.cols = new_cols
+
+    cdef void refactor_cols_down(self):
+        cdef int new_cols = self.cols * 2
+        cdef int * tmp
+        cdef int i, j
+
+        for i in range(self.rows):
+            tmp = <int *> PyMem_Malloc(new_cols*sizeof(int))
+            for j in range(self.cols):
+                # TODO: need generate map base on some probability (no cycle of wall check)
+                tmp[j] = (rand() % 3) + 1
+            for j in range(self.cols, new_cols, 1):
+                tmp[j] = self.map[i][j-self.cols]
+            PyMem_Free(self.map[i])
+            self.map[i] = tmp
+        self.pos_col += self.cols
+        self.cols = new_cols
+
+    cdef void refactor_rows_down(self):
+        cdef int new_rows = self.rows * 2
+        cdef int ** tmp
+        cdef int i, j
+        tmp = <int **> PyMem_Malloc(new_rows*sizeof(int*))
+        for i in range(self.rows):
+            tmp[i] = <int *> PyMem_Malloc(self.cols*sizeof(int))
+            for j in range(self.cols):
+                # TODO: need generate map base on some probability (no cycle of wall check)
+                tmp[i][j] = (rand() % 3) + 1
+        for i in range(self.rows, new_rows, 1):
+            tmp[i] = self.map[i-self.rows]
+        PyMem_Free(self.map)
+        self.map = tmp
+        self.pos_row += self.rows
+        self.rows = new_rows
+
+    cdef void refactor_rows_up(self):
+        cdef int new_rows = self.rows * 2
+        cdef int ** tmp
+        cdef int i, j
+        tmp = <int **> PyMem_Malloc(new_rows*sizeof(int*))
+        for i in range(self.rows):
+            tmp[i] = self.map[i]
+        for i in range(self.rows, new_rows, 1):
+            tmp[i] = <int *> PyMem_Malloc(self.cols*sizeof(int))
+            for j in range(self.cols):
+                # TODO: need generate map base on some probability (no cycle of wall check)
+                tmp[i][j] = (rand() % 3) + 1
+        PyMem_Free(self.map)
+        self.map = tmp
+        self.rows = new_rows
+
+    @cython.nonecheck(False)
+    @cython.cdivision(True)
+    cdef coords get_cords_slides(self, int x, int y):
+        cdef int x1, x2, y1, y2
+        x1 = self.pos_row - (x-1)/2
+        x2 = self.pos_row + x/2 + 1
+        y1 = self.pos_col - (y-1)/2
+        y2 = self.pos_col + y/2 + 1
+        return coords(x1, x2, y1, y2)
 
 
 @cython.boundscheck(False)
